d09ead343beec1ef480186b9c7254c701b856f0b
Bharat Sharma <21104023bharatsharma@gmail.com>
2023-09-28 08:54:25 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ async function exampleTest() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get('http://moodle.apsit.org.in/moodle/login/index.php');
-    await driver.findElement(By.name('username')).sendKeys('21104043');
-    await driver.findElement(By.name('password')).sendKeys('21104042@Apsit');
+    await driver.findElement(By.name('username')).sendKeys('Your_Moodle_ID');
+    await driver.findElement(By.name('password')).sendKeys('Your_Password');
     await driver.findElement(By.id('loginbtn')).click();
   } catch (err) {
     console.error(err);
